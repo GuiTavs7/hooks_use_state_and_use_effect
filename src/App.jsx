96df0678fef195aefba83dc9d12c83bf49ca5715
GuiTavs7 <7tavs7@gmail.com>
2025-09-06 17:42:38 -0300

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-import Panel from "./components/panel/panel";
 import DeckOfCards from './components/deck-of-cards/deck-of-cards';
 
 function App() {
--- src/components/forms/form.jsx
+++ src/components/forms/form.jsx
@@ -2,51 +2,80 @@ import { useState } from 'react';
 import './form.css';
 
 const Form = (props) => {
+  const [inputs, setInputs] = useState({
+    value: '',
+    suit: ''
+  });
 
-    const [inputs, setInputs] = useState({
-        image: '',
-        value: '',
-        suit: ''
-    })
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
 
-    const handleInputChange = (event) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        console.log(event.target.name);
+    // Mapeamento de abreviações
+    const valueMap = {
+      'A': 'A', 'J': 'J', 'Q': 'Q', 'K': 'K',
+      '10': '0', '9': '9', '8': '8', '7': '7',
+      '6': '6', '5': '5', '4': '4', '3': '3', '2': '2'
+    };
 
-        const { target } = event;
-        const { name } = target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
+    const suitMap = {
+      'espadas': 'S',
+      'copas': 'H',
+      'ouros': 'D',
+      'paus': 'C'
+    };
 
-        setInputs({
-            ...inputs,
-            [name]: value
-        })
-    }
+    const valueCode = valueMap[inputs.value.toUpperCase()] || inputs.value.toUpperCase();
+    const suitCode = suitMap[inputs.suit.toLowerCase()] || inputs.suit.toUpperCase();
 
-    const handleSubmit = (event) => {
-        event.preventDefault(); //previne a atualização da página ao enviar o formulário
-        props.addCard(inputs);
-    }
+    const code = `${valueCode}${suitCode}`;
+    const imageUrl = `https://deckofcardsapi.com/static/img/${code}.png`;
 
-    return (
-        <>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="image">Link da Imagem</label>
-                <input type="text" id="image" name="image" className="input-text" onChange={handleInputChange} value={inputs.image}/>
-            </div>
-             <div>
-                <label htmlFor="value">Nome da Carta</label>
-                <input type="text" id="value" name="value" className="input-text" onChange={handleInputChange} value={inputs.value}/>
-            </div>
-             <div>
-                <label htmlFor="suit">Naipe da Carta</label>
-                <input type="text" id="suit" name="suit" className="input-text" onChange={handleInputChange} value={inputs.suit}/>
-            </div>
-            <input type="submit" className="btn-submit"/>
-        </form>
-        </>
-    )
-}
+    const newCard = {
+      image: imageUrl,
+      value: inputs.value,
+      suit: inputs.suit
+    };
+
+    props.addCard(newCard);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="value">Nome da Carta</label>
+        <input
+          type="text"
+          id="value"
+          name="value"
+          placeholder="Ex: A, K, Q, J, 10..."
+          onChange={handleInputChange}
+          value={inputs.value}
+          className="input-text"
+        />
+      </div>
+      <div>
+        <label htmlFor="suit">Naipe da Carta</label>
+        <input
+          type="text"
+          id="suit"
+          name="suit"
+          placeholder="S, H, D, C ou espadas, etc..."
+          onChange={handleInputChange}
+          value={inputs.suit}
+          className="input-text"
+        />
+      </div>
+      <input type="submit" value="Adicionar Carta" className="btn-submit" />
+    </form>
+  );
+};
 
 export default Form;
